bf36cfe00d0ea8825397349d208551827b2337ef
WhosDustin <dustin@delatore.me>
2015-08-07 21:34:25 -0400

    commited changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -298,7 +298,7 @@ module.exports = function (grunt) {
       dist: {
         options: {
           remote: 'https://github.com/whosdustin/whosdustin.github.io.git',
-          branch: 'gh-pages',
+          branch: 'master',
           commit: true,
           push: true
         }
