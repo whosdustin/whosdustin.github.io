069b4994e5e5e900f2349a0f84fc4f2bae1b6627
WhosDustin <dustin@delatore.me>
2015-12-09 22:44:11 -0500

    rename imagemin

--- grunt/buildcontrol.js
+++ grunt/buildcontrol.js
@@ -2,7 +2,7 @@ module.exports = {
   dist: {
     options: {
       remote: 'https://github.com/whosdustin/whosdustin.github.io.git',
-      branch: 'master',
+      branch: 'gh-pages',
       commit: true,
       push: true,
       message: '🚀Launched from commit %sourceCommit%'
