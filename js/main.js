14eeb41f353dcb8644651789c817cb81480d65d9
WhosDustin <dustin@delatore.me>
2016-08-13 07:52:35 -0400

    Mario lol

--- js/main.js
+++ js/main.js
@@ -13,3 +13,8 @@
 // window.setTimeout(function() {
 // 	document.getElementsByTagName('head')[0].appendChild(styleNode)
 // }, 5000);
+
+var listener = new window.keypress.Listener();
+listener.sequence_combo('m a r i o', function() {
+	// window.innerHTML = "<div class=\"mario__path\"><img class=\"mario\" src=\"/images/img-mario-running.gif\" /></div>";
+}, true);
