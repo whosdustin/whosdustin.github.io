83e1dcf94bc58d763c61317822ed00b6a8144fce
WhosDustin <dustin@delatore.me>
2016-08-13 16:27:34 -0400

    type mario on the screen

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
-// Widow be gone.
-// var headings = document.getElementsByTagName( 'h1' );
-// for ( var i=0; i<headings.length; i++ ) {
-//   var h1s = headings[i].innerHTML.split( ' ' );
-//   h1s[h1s.length-2] += "&nbsp;" + h1s[h1s.length-1];
-//   h1s.pop();
-//   headings[i].innerHTML = h1s.join( ' ' );
-// }
-
-// var styleNode = document.createElement('link');
-// styleNode.rel = "stylesheet";
-// styleNode.setAttribute("href", "/css/color.css");
-// window.setTimeout(function() {
-// 	document.getElementsByTagName('head')[0].appendChild(styleNode)
-// }, 5000);
+// Remove double tap on ios for hover links
+if (!("ontouchstart" in document.documentElement)) {
+	document.documentElement.className += " no-touch";
+}
 
 var listener = new window.keypress.Listener();
 listener.sequence_combo('m a r i o', function() {
-	// window.innerHTML = "<div class=\"mario__path\"><img class=\"mario\" src=\"/images/img-mario-running.gif\" /></div>";
+	var marioPath = document.getElementById('marioPath');
+	marioPath.innerHTML += '<img id="mario" class="mario" src="/images/img-mario-running.gif" />';
+	var marioWait = setTimeout(function() {
+		var mario = document.getElementById('mario');
+		marioPath.removeChild(mario);
+	}, 5000);
 }, true);
