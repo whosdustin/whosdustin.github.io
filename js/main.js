6abd8beb9d6197411e1f3a9c137c08359628d042
WhosDustin <dustin@delatore.me>
2016-08-20 14:11:35 -0400

    Add and remove entire mario path

--- js/main.js
+++ js/main.js
@@ -5,10 +5,9 @@ if (!("ontouchstart" in document.documentElement)) {
 
 var listener = new window.keypress.Listener();
 listener.sequence_combo('m a r i o', function() {
-	var marioPath = document.getElementById('marioPath');
-	marioPath.innerHTML += '<img id="mario" class="mario" src="/images/img-mario-running.gif" />';
+	document.body.innerHTML += '<div id="marioPath" class="mario__path"><img id="mario" class="mario" src="/images/img-mario-running.gif" /></div>';
 	var marioWait = setTimeout(function() {
-		var mario = document.getElementById('mario');
-		marioPath.removeChild(mario);
+		var mario = document.getElementById('marioPath');
+		document.body.removeChild(mario);
 	}, 5000);
 }, true);
